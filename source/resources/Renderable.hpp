f25105a9a2e8497ed674b74e587e0142a4725b94
Peter Hillerström <peter@hstroem.se>
2016-08-08 21:06:33 +0200

    Added tinyobjloader dependency and can now load very basic .obj files.

--- source/resources/Renderable.hpp
+++ source/resources/Renderable.hpp
@@ -1,1 +1,37 @@
 // Copyright (c) Peter Hillerström (skipifzero.com, peter@hstroem.se)
+
+#pragma once
+
+#include "resources/GLModel.hpp"
+#include "resources/RawGeometry.hpp"
+
+namespace sfz {
+
+// Renderable class
+// ------------------------------------------------------------------------------------------------
+
+class Renderable final {
+public:
+	// Members
+	// --------------------------------------------------------------------------------------------
+
+	RawGeometry geometry;
+	GLModel glModel;
+
+	// Constructors & destructors
+	// --------------------------------------------------------------------------------------------
+
+	Renderable() noexcept = default;
+	Renderable(const Renderable&) = delete;
+	Renderable& operator= (const Renderable&) = delete;
+	Renderable(Renderable&&) noexcept = default;
+	Renderable& operator= (Renderable&&) noexcept = default;
+	~Renderable() noexcept = default;
+};
+
+// Renderable creation functions
+// ------------------------------------------------------------------------------------------------
+
+Renderable tinyObjLoadRenderable(const char* basePath, const char* fileName) noexcept;
+
+} // namespace sfz
