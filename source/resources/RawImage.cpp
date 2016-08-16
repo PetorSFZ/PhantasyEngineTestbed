eee8a505c7d9557956fb21409ab9cb904a19dba6
Peter Hillerström <peter@hstroem.se>
2016-08-17 00:56:53 +0200

    RawImage: Simplified and added function to load using stb_image

--- source/resources/RawImage.cpp
+++ source/resources/RawImage.cpp
@@ -4,6 +4,9 @@
 
 #include <cstring> // std::memcpy
 
+#include <stb_image.h>
+
+#include <sfz/containers/DynString.hpp>
 #include <sfz/memory/Allocators.hpp>
 
 namespace sfz {
@@ -13,26 +16,22 @@ namespace sfz {
 
 void RawImage::flipVertically() noexcept
 {
-	sfz_assert_debug(mData.data() != nullptr);
-	uint8_t* buffer = static_cast<uint8_t*>(StandardAllocator::allocate(mPitch));
-	for (int32_t i = 0; i < (mDim.y/2); i++) {
-		uint8_t* begin = mData.data() + i * mPitch;
-		uint8_t* end = mData.data() + (mDim.y - i - 1) * mPitch;
+	sfz_assert_debug(imgData.data() != nullptr);
+	uint8_t* buffer = static_cast<uint8_t*>(StandardAllocator::allocate(pitch));
+	for (int32_t i = 0; i < (dim.y / 2); i++) {
+		uint8_t* begin = imgData.data() + i * pitch;
+		uint8_t* end = imgData.data() + (dim.y - i - 1) * pitch;
 
-		std::memcpy(buffer, begin, mPitch);
-		std::memcpy(begin, end, mPitch);
-		std::memcpy(end, buffer, mPitch);
+		std::memcpy(buffer, begin, pitch);
+		std::memcpy(begin, end, pitch);
+		std::memcpy(end, buffer, pitch);
 	}
 	StandardAllocator::deallocate(buffer);
 }
 
-// RawImage: Getters
-// ------------------------------------------------------------------------------------------------
-
 uint8_t* RawImage::getPixelPtr(int32_t x, int32_t y) noexcept
 {
-	uint8_t* img = this->data();
-	return img + (uint32_t(y) * mPitch + uint32_t(x) * mBytesPerPixel);
+	return imgData.data() + (uint32_t(y) * pitch + uint32_t(x) * bytesPerPixel);
 }
 
 uint8_t* RawImage::getPixelPtr(vec2i location) noexcept
@@ -42,13 +41,47 @@ uint8_t* RawImage::getPixelPtr(vec2i location) noexcept
 
 const uint8_t* RawImage::getPixelPtr(int32_t x, int32_t y) const noexcept
 {
-	const uint8_t* img = this->data();
-	return img + (uint32_t(y) * mPitch + uint32_t(x) * mBytesPerPixel);
+	return imgData.data() + (uint32_t(y) * pitch + uint32_t(x) * bytesPerPixel);
 }
 
 const uint8_t* RawImage::getPixelPtr(vec2i location) const noexcept
 {
 	return this->getPixelPtr(location.x, location.y);
+}
+
+RawImage loadImage(const char* basePath, const char* fileName) noexcept
+{
+	if (basePath == nullptr && fileName == nullptr) {
+		printErrorMessage("RawImage: Invalid path to image");
+		return RawImage();
+	}
+
+	// Concatenate path
+	size_t basePathLen = std::strlen(basePath);
+	size_t fileNameLen = std::strlen(fileName);
+	DynString path("", uint32_t(basePathLen + fileNameLen + 2));
+	path.printf("%s%s", basePath, fileName);
+
+	// Loading image
+	int width, height, numChannels;
+	uint8_t* img = stbi_load(path.str(), &width, &height, &numChannels, 0);
+
+	// Error checking
+	if (img == nullptr) {
+		printErrorMessage("RawImage: Unable to load image \"%s\", reason: %s", fileName, stbi_failure_reason());
+		return RawImage();
+	}
+
+	// Create RawImage from data
+	RawImage tmp;
+	tmp.dim = vec2i(width, height);
+	tmp.bytesPerPixel = uint32_t(numChannels);
+	tmp.pitch = uint32_t(width * numChannels);
+	tmp.imgData.add(img, uint32_t(width * height * numChannels));
+
+	// Free data and return RawImage
+	stbi_image_free(img);
+	return std::move(tmp);
 }
 
 } // namespace sfz
--- source/resources/RawImage.hpp
+++ source/resources/RawImage.hpp
@@ -10,51 +10,31 @@ namespace sfz {
 // RawImage
 // ------------------------------------------------------------------------------------------------
 
-/// A class holding a raw image in memory
-class RawImage final {
-public:
-	// Constructors & destructors
+/// A struct holding a raw image in memory
+struct RawImage final {
+	// Members
 	// --------------------------------------------------------------------------------------------
 
-	RawImage() noexcept = default;
-	RawImage(const RawImage&) noexcept = default;
-	RawImage& operator= (const RawImage&) noexcept = default;
-	RawImage(RawImage&&) noexcept = default;
-	RawImage& operator= (RawImage&&) noexcept = default;
-	~RawImage() noexcept = default;
+	DynArray<uint8_t> imgData;
+	vec2i dim = vec2i(0);
+	uint32_t bytesPerPixel = 0;
+	uint32_t pitch = 0; // Length of row in bytes
 
 	// Methods
 	// --------------------------------------------------------------------------------------------
 
-	inline bool isEmpty() const noexcept { return mData.data() == nullptr; }
-	
+	inline bool isEmpty() const noexcept { return imgData.data() == nullptr; }
+
 	/// Flips the image vertically, i.e. the top row will become the bottom row, etc.
 	void flipVertically() noexcept;
-
-	// Getters
-	// --------------------------------------------------------------------------------------------
-
-	inline uint8_t* data() noexcept { return mData.data(); }
-	inline const uint8_t* data() const noexcept { return mData.data(); }
-	inline vec2i dimensions() const noexcept { return mDim; }
-	inline int32_t width() const noexcept { return mDim.x; }
-	inline int32_t height() const noexcept { return mDim.y; }
-	inline uint32_t bytesPerPixel() const noexcept { return mBytesPerPixel; }
-	inline uint32_t pitch() const noexcept { return mPitch; }
 
 	uint8_t* getPixelPtr(int32_t x, int32_t y) noexcept;
 	uint8_t* getPixelPtr(vec2i location) noexcept;
 	const uint8_t* getPixelPtr(int32_t x, int32_t y) const noexcept;
 	const uint8_t* getPixelPtr(vec2i location) const noexcept;
-
-private:
-	// Private members
-	// --------------------------------------------------------------------------------------------
-
-	DynArray<uint8_t> mData;
-	vec2i mDim = vec2i(0);
-	uint32_t mBytesPerPixel = 0;
-	uint32_t mPitch = 0; // Length of row in bytes
 };
+
+/// Loads a RawImage from file using stb_image
+RawImage loadImage(const char* basePath, const char* fileName) noexcept;
 
 } // namespace sfz
