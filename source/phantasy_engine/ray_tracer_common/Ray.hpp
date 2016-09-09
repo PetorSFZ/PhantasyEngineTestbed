28cefb810495f473477f78a9f82b4db661ec8fed
Peter Hillerström <peter@hstroem.se>
2016-09-09 20:36:18 +0200

    Removed RayTracerCommonSharedHeader, use sfzCore support instead.

--- source/phantasy_engine/ray_tracer_common/Ray.hpp
+++ source/phantasy_engine/ray_tracer_common/Ray.hpp
@@ -2,37 +2,38 @@
 
 #pragma once
 
-#include "phantasy_engine/ray_tracer_common/RayTracerCommonSharedHeader.hpp"
+#include <sfz/CUDACallable.hpp>
+#include <sfz/math/Vector.hpp>
 
 namespace phe {
 
+using sfz::vec3;
+
 struct Ray {
-	vec3_t origin;
-	vec3_t dir;
-	vec3_t invDir;
+	vec3 origin;
+	vec3 dir;
+	vec3 invDir;
 
 	Ray() noexcept = default;
 	Ray(const Ray&) noexcept = default;
 	Ray& operator= (const Ray&) noexcept = default;
 	~Ray() noexcept = default;
 
-	PHE_CUDA_AVAILABLE Ray(const vec3_t& originIn, const vec3_t& directionIn) noexcept
+	SFZ_CUDA_CALLABLE Ray(const vec3& originIn, const vec3& directionIn) noexcept
 	{
 		this->origin = originIn;
 		this->dir = directionIn;
 	}
 
-	PHE_CUDA_AVAILABLE void setOrigin(const vec3_t& originIn) noexcept
+	SFZ_CUDA_CALLABLE void setOrigin(const vec3& originIn) noexcept
 	{
 		this->origin = originIn;
 	}
 
-	PHE_CUDA_AVAILABLE void setDir(const vec3_t& dirIn) noexcept
+	SFZ_CUDA_CALLABLE void setDir(const vec3& dirIn) noexcept
 	{
 		this->dir = dirIn;
-		this->invDir.x = 1.0f / dirIn.x;
-		this->invDir.y = 1.0f / dirIn.y;
-		this->invDir.z = 1.0f / dirIn.z;
+		this->invDir = vec3(1.0f) / dirIn;
 	}
 };
 
