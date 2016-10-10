24e2ea29070c56661ed0e83e85c40075042c8476
Johan Hasselqvist <johan_hasselqvist@hotmail.com>
2016-10-10 16:11:38 +0200

    STARS!

--- source/testbed_application/TestbedLogic.cpp
+++ source/testbed_application/TestbedLogic.cpp
@@ -50,7 +50,7 @@ UpdateOp TestbedLogic::update(GameScreen& screen, UpdateState& state) noexcept
 	// Move balls
 	for (uint32_t handle : instanceHandles) {
 		vec3& pos = objectPositions[handle];
-		vec3 velocity = vec3(0.0f, cos(accumulatedTime), (sin(accumulatedTime) - cos(accumulatedTime/4))) * 10.0f * state.delta;
+		vec3 velocity = vec3(0.0f, cos(accumulatedTime) - sin(accumulatedTime/4), sin(accumulatedTime) - cos(accumulatedTime/4)) * 10.0f * state.delta;
 		pos += velocity;
 		screen.level->objects[handle].transform = translationMatrix(pos);
 	}
