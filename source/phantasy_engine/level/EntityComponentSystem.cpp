ce8c886dc760ec0ed29a3489b4fd09dfb10e4cf6
Peter Hillerström <peter@hstroem.se>
2016-11-07 23:27:44 +0100

    More work on EntityComponentSystem

--- source/phantasy_engine/level/EntityComponentSystem.cpp
+++ source/phantasy_engine/level/EntityComponentSystem.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 
+#include <sfz/Assert.hpp>
 #include <sfz/containers/DynArray.hpp>
 #include <sfz/memory/New.hpp>
 
@@ -14,17 +15,42 @@ using namespace sfz;
 // EntityComponentSystemImpl
 // ------------------------------------------------------------------------------------------------
 
+struct ComponentStruct {
+	DynArray<uint8_t> data;
+	uint32_t bytesPerComponent;
+};
+
 class EntityComponentSystemImpl final {
 public:
+	// The maximum number of entities allowed
+	uint32_t maxNumEntities;
 
-	uint32_t maxNumEntities = 0;
-	uint32_t maxNumComponents = 0;
-	uint32_t currentNumComponents = 0;
-	DynArray<DynArray<uint8_t>> data;
+	// The current number of entities
+	uint32_t currentNumEntities = 0;
+
+	//The current number of component types
+	uint32_t currentNumComponentTypes = 0;
+
+	// All currently free entity indices in this system
+	DynArray<uint32_t> freeEntitiesStack;
+
+
+
+	DynArray<uint8_t> mask;
+	DynArray<ComponentStruct> components;
 
 
 	EntityComponentSystemImpl(uint32_t maxNumEntities) noexcept
 	{
+		this->maxNumEntities = maxNumEntities;
+
+		// Create and fill freeEntities with all free indices
+		freeEntitiesStack.setCapacity(maxNumEntities);
+		freeEntitiesStack.setSize(maxNumEntities);
+		for (uint32_t i = 0; i < maxNumEntities; i++) {
+			freeEntitiesStack[i] = maxNumEntities - i - 1u;
+		}
+
 
 	}
 
@@ -77,30 +103,50 @@ uint32_t EntityComponentSystem::maxNumEntities() const noexcept
 	return mImpl->maxNumEntities;
 }
 
-uint32_t EntityComponentSystem::maxNumComponents() const noexcept
+uint32_t EntityComponentSystem::currentNumEntities() const noexcept
 {
-	return mImpl->maxNumComponents;
+	return mImpl->currentNumEntities;
 }
 
-uint32_t EntityComponentSystem::currentNumComponents() const noexcept
+uint32_t EntityComponentSystem::currentNumComponentTypes() const noexcept
 {
-	return mImpl->currentNumComponents;
+	return mImpl->currentNumComponentTypes;
+}
+
+// EntityComponentSystem: Entity creation/deletion
+// ------------------------------------------------------------------------------------------------
+
+uint32_t EntityComponentSystem::createEntity() noexcept
+{
+	sfz_assert_release(mImpl->freeEntitiesStack.size() > 0);
+	uint32_t entity = mImpl->freeEntitiesStack.last();
+	mImpl->freeEntitiesStack.removeLast();
+	return entity;
+}
+
+void EntityComponentSystem::deleteEntity(uint32_t entity) noexcept
+{
+	// TODO:
+	// - Remove all associated components
+	// - Clear associated mask
+	// - Add to freeEntitiesStack
 }
 
 // EntityComponentSystem: Raw (non-typesafe) methods
 // ------------------------------------------------------------------------------------------------
 
-void* EntityComponentSystem::getComponentRaw(uint32_t entity, uint32_t componentType,
-                                             uint32_t bytesPerComponent) noexcept
+uint32_t EntityComponentSystem::createComponentTypeRaw(uint32_t bytesPerComponent) noexcept
 {
-	// TODO: Implement
+	return 0;
+}
+
+void* EntityComponentSystem::getComponentRaw(uint32_t entity, uint32_t componentType) noexcept
+{
 	return nullptr;
 }
 
-const void* EntityComponentSystem::getComponentRaw(uint32_t entity, uint32_t componentType,
-                                                   uint32_t bytesPerComponent) const noexcept
+const void* EntityComponentSystem::getComponentRaw(uint32_t entity, uint32_t componentType) const noexcept
 {
-	// TODO: Implement
 	return nullptr;
 }
 
--- source/phantasy_engine/level/EntityComponentSystem.hpp
+++ source/phantasy_engine/level/EntityComponentSystem.hpp
@@ -15,6 +15,11 @@ class EntityComponentSystemImpl; // Pimpl
 
 class EntityComponentSystem final {
 public:
+	// Constants
+	// --------------------------------------------------------------------------------------------
+
+	constexpr static uint32_t MAX_NUM_COMPONENT_TYPES = 128u;
+
 	// Constructors & destructors
 	// --------------------------------------------------------------------------------------------
 
@@ -30,21 +35,49 @@ public:
 	// State methods & getters
 	// --------------------------------------------------------------------------------------------
 
+	/// Destroys this EntityComponentSystem, called automatically in the destructor.
 	void destroy() noexcept;
+
+	/// Swaps the contents of this EntityComponentSystem with another, same as using a move
+	/// constructor.
 	void swap(EntityComponentSystem& other) noexcept;
+
+	/// Checks whether this EntityComponentSystem is valid or not. If it is not valid then all
+	/// methods except for destroy() and swap() is undefined and should under no circumstances be
+	/// called.
 	bool isValid() const noexcept { return mImpl != nullptr; }
 
+	/// Returns the maximum number of entites allowed by this system
 	uint32_t maxNumEntities() const noexcept;
-	uint32_t maxNumComponents() const noexcept;
-	uint32_t currentNumComponents() const noexcept;
+
+	/// Returns the current number of entities in this system
+	uint32_t currentNumEntities() const noexcept;
+
+	/// Returns the current number of component types in this system. The maximum number is defined
+	/// by the MAX_NUM_COMPONENT_TYPES constant.
+	uint32_t currentNumComponentTypes() const noexcept;
+
+	// Entity creation/deletion
+	// --------------------------------------------------------------------------------------------
+
+	/// Creates a new entity with no associated components. Index is guaranteed to be smaller than
+	/// the systems maximum number of entities. Indices used for removed entities will be used
+	uint32_t createEntity() noexcept;
+
+	/// Deletes an entity. Will remove all associated components and free the index to be reused
+	/// for future entities.
+	void deleteEntity(uint32_t entity) noexcept;
 
 	// Raw (non-typesafe) methods
 	// --------------------------------------------------------------------------------------------
 
-	void* getComponentRaw(uint32_t entity, uint32_t componentType,
-	                      uint32_t bytesPerComponent) noexcept;
-	const void* getComponentRaw(uint32_t entity, uint32_t componentType,
-	                            uint32_t bytesPerComponent) const noexcept;
+	/// Specifies a new type of component, returns its index to be used when accessing
+	uint32_t createComponentTypeRaw(uint32_t bytesPerComponent) noexcept;
+	
+	/// Returns pointer to the component of specified type for a given entity. Returns nullptr if
+	/// it does not exist.
+	void* getComponentRaw(uint32_t entity, uint32_t componentType) noexcept;
+	const void* getComponentRaw(uint32_t entity, uint32_t componentType) const noexcept;
 
 private:
 	// Private members
@@ -52,24 +85,5 @@ private:
 
 	EntityComponentSystemImpl* mImpl = nullptr;
 };
-
-inline size_t &component_counter()
-{
-	static size_t counter = 0;
-	return counter;
-}
-
-template<typename C>
-size_t component_index()
-{
-	static size_t index = inc_component_counter();
-	return index;
-}
-
-inline size_t inc_component_counter()
-{
-	size_t index = component_counter()++;
-	return index;
-}
 
 } // namespace phe
